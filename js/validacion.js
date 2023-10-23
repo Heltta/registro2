d2722b7885dd1cd55664318d51928b9050d07144
Heltta <lucashelgueraz@gmail.com>
2023-10-22 22:18:29 -0300

    Comparacion de contrasenias
    Co-authored-by: florencia190 <florencia190@users.noreply.github.com>
    Co-authored-by: Damián Romero <d-romero-dev@users.noreply.github.com>
    Co-authored-by: carolinasf23 <carolinasf23@users.noreply.github.com>

--- js/validacion.js
+++ js/validacion.js
@@ -8,57 +8,39 @@ const campos = {
   terminos: document.getElementById('terminos'),
 };
 
-function contraseñademasiadoCorta() {
-  return campos.password1.value.length <= 6;
-}
-
-function esIgual() {
-  return campos.password1.value == campos.password2.value;
-}
-
-// function ningunVacio() {
-//   for (const key in campos) {
-//     if (!campos[key].value) {
-//       return false;
-//     }
-//   }
-// }
-
-function showAlertSuccess() {
-  document.getElementById('alert-success').classList.add('show');
-}
-
-function showAlertError() {
-  document.getElementById('alert-danger').classList.add('show');
-}
-
-(document.getElementsByTagName("form"))[0].addEventListener('submit', (event) => {
-  if (!event.checkValidity() || contraseñademasiadoCorta() || !esIgual()) {
-    event.preventDefault()
-    showAlertError();
+function checkPassword() {
+  if (campos.password1.value !== campos.password2.value) {
+    campos.password1.setCustomValidity('Las contraseña es incorrecta');
+    campos.password2.setCustomValidity('Las contraseña es incorrecta');
   } else {
-    showAlertSuccess();
+    campos.password1.setCustomValidity('');
+    campos.password2.setCustomValidity('');
   }
-});
+}
 
+campos.password1.addEventListener('input', checkPassword);
+campos.password2.addEventListener('input', checkPassword);
 
 // Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
 (function () {
-    'use strict'
-  
-    // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
-    var forms = document.querySelectorAll('.needs-validation')
-  
-    // Bucle sobre ellos y evitar el envío
-    Array.prototype.slice.call(forms)
-      .forEach(function (form) {
-        form.addEventListener('submit', function (event) {
-          if (!form.checkValidity() && campos.terminos.checkValidity()) {
-            event.preventDefault()
-            event.stopPropagation()
-          }
-  
-          form.classList.add('was-validated')
-        }, false)
-      })
-  })()
+  'use strict';
+
+  // Obtener todos los formularios a los que queremos aplicar estilos de validación de Bootstrap personalizados
+  var forms = document.querySelectorAll('.needs-validation');
+
+  // Bucle sobre ellos y evitar el envío
+  Array.prototype.slice.call(forms).forEach(function (form) {
+    form.addEventListener(
+      'submit',
+      function (event) {
+        if (!form.checkValidity() && campos.terminos.checkValidity()) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+
+        form.classList.add('was-validated');
+      },
+      false
+    );
+  });
+})();
