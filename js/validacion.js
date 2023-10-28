d5b166c05179ee86586b823c0351c9db13f43411
Heltta <lucashelgueraz@gmail.com>
2023-10-28 19:12:53 -0300

    Feedback btn y feedback equal password on submit

--- js/validacion.js
+++ js/validacion.js
@@ -6,9 +6,31 @@ const campos = {
   password1: document.getElementById('password1'),
   password2: document.getElementById('password2'),
   terminos: document.getElementById('terminos'),
+  btnTerminos: document.getElementById('buttonTerminosServicio'),
 };
 
-function checkPassword() {
+/**
+ * Se le da un elemento de control de formulario para checkear su validacion
+ * y un elemento que al que también debe mostrar el feedback de la validación
+ * retorna true si la validacion es exitosa y false si no es válido
+ *
+ * @param {HTMLElement} domCompanieroDeFeedback Elemento que carece de validacion propia pero se le debe adjuntar un feedback identico al del otro elemento
+ * @param {HTMLElement} domCampoParaValidar Elemento que tiene tiene requerimientos de validacion
+ * @returns {Boolean}
+ */
+function validacionConFeedbackEnDosElementos(
+  domCompanieroDeFeedback,
+  domCampoParaValidar
+) {
+  const esValido = domCampoParaValidar.checkValidity();
+  domCompanieroDeFeedback.classList.remove(
+    !esValido ? 'is-valid' : 'is-invalid'
+  );
+  domCompanieroDeFeedback.classList.add(esValido ? 'is-valid' : 'is-invalid');
+  return esValido;
+}
+
+function checkEqualPasswords() {
   if (campos.password1.value !== campos.password2.value) {
     campos.password1.setCustomValidity('La contraseña es incorrecta');
     campos.password2.setCustomValidity('La contraseña es incorrecta');
@@ -18,8 +40,11 @@ function checkPassword() {
   }
 }
 
-campos.password1.addEventListener('input', checkPassword);
-campos.password2.addEventListener('input', checkPassword);
+campos.password1.addEventListener('input', checkEqualPasswords);
+campos.password2.addEventListener('input', checkEqualPasswords);
+campos.terminos.addEventListener('input', (event) => {
+  validacionConFeedbackEnDosElementos(campos.btnTerminos, event.target);
+});
 
 // Ejemplo de JavaScript inicial para deshabilitar el envío de formularios si hay campos no válidos
 (function () {
@@ -33,7 +58,12 @@ campos.password2.addEventListener('input', checkPassword);
     form.addEventListener(
       'submit',
       function (event) {
-        if (!form.checkValidity() && campos.terminos.checkValidity()) {
+        checkEqualPasswords();
+        const terminosChecked = validacionConFeedbackEnDosElementos(
+          document.getElementById('buttonTerminosServicio'),
+          campos.terminos
+        );
+        if (!form.checkValidity() || !terminosChecked) {
           event.preventDefault();
           event.stopPropagation();
         }
